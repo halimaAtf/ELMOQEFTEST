b2a89d5b1bd282ccb4f722486e9d1e48b495b7db
halima ataifa <halima.ataifa125@gmail.com>
2026-02-14 02:24:38 +0100

    "ajouter Email"

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -23,4 +23,13 @@ public class User {
     public void setId(Long id) { this.id = id; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
+    public String getEmail() {return Email;}
+    public void setEmail(String Email){this.Email = Email;}
+    public String getPhone() {return phone;}
+    public void setPhone(String phone){this.phone = phone;}
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+    public boolean isValidated() { return isValidated; }
+    public void setValidated(boolean validated) { isValidated = validated; }
+
 }
